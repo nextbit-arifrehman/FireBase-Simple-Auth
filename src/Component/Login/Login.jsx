18bb40246b02104e64d68b09207b6a6436a1f410
nextbit-arifrehman <asustufk513ea@gmail.com>
2025-04-26 03:03:24 +0600

    SignIn vs SignOUt

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,54 +1,73 @@
-import React, { useState } from 'react';
-import{auth} from '../../firebase-js';
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-
+import React, { useState } from "react";
+import { auth } from "../../firebase-js";
+import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 
 const Login = () => {
+  const [user, setUser] = useState(null);
 
-const [user, setUser] = useState(null);
+  const Gprovider = new GoogleAuthProvider();
 
-const Gprovider = new GoogleAuthProvider();
+  const Logins = () => {
+    console.log("Google Sign In Click");
 
+    signInWithPopup(auth, Gprovider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const loginsOut = () => {
+    signOut(auth)
+      .then(() => {
+        console.log("sign out succesful");
+        setUser(null);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-const Logins = () => {
-  console.log("Google Sign In Click");
+  return (
+    <>
+      <div>
+        <h2>Please Login</h2>
+        {/* <button onClick={Logins} className="btn btn-active">
+          Sign In With Google
+        </button>
 
- signInWithPopup(auth, Gprovider)
-.then(result => {
-console.log(result.user);
-setUser(result.user)
+        <button onClick={loginsOut}>Sign Out</button>
+         */}
 
-}).catch(error => {
-console.log(error);
- })
- 
-};
+         {/* below one is best according to me  */}
 
-    return (
-      <>
-        <div>
-          <h2>Please Login</h2>
+        {user ? (
+          <button onClick={loginsOut}>Sign Out</button>
+        ) : (
           <button onClick={Logins} className="btn btn-active">
             Sign In With Google
           </button>
+        )}
 
-          {/* conditional format is another way */}
-          {user && (
-            <>
-              <h2>{user?.displayName}</h2>
-              <h3>{user?.email}</h3>
-              <img src={user.photoURL} alt="" />
-            </>
-          )}
+        {/* conditional format is another way */}
+        {user && (
+          <>
+            <h2>{user?.displayName}</h2>
+            <h3>{user?.email}</h3>
+            <img src={user.photoURL} alt="" />
+          </>
+        )}
 
-          {/* optional chain one way of implement*/}
-          {/* <h2>{user?.displayName}</h2>
+        {/* optional chain one way of implement*/}
+        {/* <h2>{user?.displayName}</h2>
           <h3>{user?.email}</h3> */}
-        </div>
+      </div>
 
-        <div></div>
-      </>
-    );
+      <div></div>
+    </>
+  );
 };
 
 export default Login;
