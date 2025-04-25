e3eee36de092dc72156f76f5dbdaf9ef4b7891b5
nextbit-arifrehman <asustufk513ea@gmail.com>
2025-04-26 02:50:46 +0600

    Conditional Rendering

--- src/Component/Login/Login.jsx
+++ src/Component/Login/Login.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import{auth} from '../../firebase-js';
 import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 
 
 const Login = () => {
+
+const [user, setUser] = useState(null);
 
 const Gprovider = new GoogleAuthProvider();
 
@@ -14,6 +16,8 @@ const Logins = () => {
  signInWithPopup(auth, Gprovider)
 .then(result => {
 console.log(result.user);
+setUser(result.user)
+
 }).catch(error => {
 console.log(error);
  })
@@ -21,10 +25,29 @@ console.log(error);
 };
 
     return (
+      <>
         <div>
-            <h2>Please Login</h2>
-            <button onClick={Logins} className='btn btn-active' >Sign In With Google</button>
+          <h2>Please Login</h2>
+          <button onClick={Logins} className="btn btn-active">
+            Sign In With Google
+          </button>
+
+          {/* conditional format is another way */}
+          {user && (
+            <>
+              <h2>{user?.displayName}</h2>
+              <h3>{user?.email}</h3>
+              <img src={user.photoURL} alt="" />
+            </>
+          )}
+
+          {/* optional chain one way of implement*/}
+          {/* <h2>{user?.displayName}</h2>
+          <h3>{user?.email}</h3> */}
         </div>
+
+        <div></div>
+      </>
     );
 };
 
